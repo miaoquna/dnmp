478718e4ddcec61c5c0aaf8f83c0483329ca17c3
姜梦林 <jiangmenglin@top-list.com.cn>
2018-06-25 18:36:42 +0800

    ok

--- wwwroot/site1/index.php
+++ wwwroot/site1/index.php
@@ -1,10 +1,31 @@
 <?php
 ini_set('display_errors', 'on');
-1231312
 
-$redis = new Redis();
-$redis->connect('dnmp.redis', 6379);
-$redis->set('test', 'hello docker redis');
-echo $redis->get('test');
-$redis->set('test', 'hello docker redis1111111111111');
-echo $redis->get('test');
+try {
+    $mysql = new mysqli('dnmp.mysql', 'root', '123456', 'mysql');
+    if ($mysql->connect_error) {
+        die('Connect Error (' . $db->connect_errno . ') ' . $mysqli->connect_error);
+    }
+    $mysql->close();
+    echo "hello docker mysql！<br/>";
+} catch (Exception $exception) {
+    echo $exception->getMessage()."<br/>";
+}
+
+try {
+    $redis = new Redis();
+    $redis->connect('dnmp.redis', 6379);
+    $redis->set('test', 'hello docker redis！');
+    echo $redis->get('test')."<br/>";
+} catch (Exception $exception) {
+    echo $exception->getMessage()."<br/>";
+}
+
+try {
+    $memcached = new Memcached('mc');
+    $memcached->addServer('dnmp.memcached', '11211');
+    $redis->set('test', 'hello docker memcached！');
+    echo $redis->get('test')."<br/>";
+} catch (Exception $exception) {
+    echo $exception->getMessage()."<br/>";
+}
